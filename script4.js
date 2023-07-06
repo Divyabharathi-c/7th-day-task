ab6894ed7e90a023370b98f3df05403623e13184
Divyabharathi-c <137372292+Divyabharathi-c@users.noreply.github.com>
2023-07-06 13:35:45 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,8 @@
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const population=countryData.reduce((acc,element)=>{
+        return acc+element.population;
+    },0)
+    console.log(population);}
